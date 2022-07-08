ae1912b51c82ce0d955372060eaa5a16f5778a09
Bruno Lucattelli <lucattelli@gmail.com>
2022-07-07 21:12:25 -0300

    Fresh start

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
-fn should_slack_send_a_notification(channel_is_muted: bool) -> bool {
-    !channel_is_muted
-}
 
-#[test]
-fn test_channel_is_muted_do_not_send_notification() {
-    let channel_is_muted = true;
-    assert_eq!(should_slack_send_a_notification(channel_is_muted), false)
-}
